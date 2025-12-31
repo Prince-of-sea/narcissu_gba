d5e9890d22760ea95a5d7a3cc1d49acf59178de4
PC-CNT <49097489+PC-CNT@users.noreply.github.com>
2025-12-31 19:44:37 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ cd ./src/c/game_core
 export PATH=$PATH:/opt/devkitpro/devkitARM/bin
 make clean
 make MESSAGE="Test" TARGET=test
-make clean
+# make clean
 # make MESSAGE="Hi" TARGET=hi
 # make clean
