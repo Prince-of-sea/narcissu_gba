eabb23904dd1bd8fa769d4c87bfb40c905f6406b
Prince-of-sea <94970975+Prince-of-sea@users.noreply.github.com>
2026-01-10 02:50:13 +0900

    容量チェック

--- src/python/converter/core/file_checker.py
+++ src/python/converter/core/file_checker.py
@@ -12,17 +12,25 @@ def check_files(cfg: AppConfig) -> None:
     r = True
 
     for f_info in FILE_CRC32_LIST:
-        f_path = cfg.input_dir / Path(f_info[1])
+        f_path = cfg.input_dir / Path(f_info[2])
         if not f_path.exists():
             r = False
             print(f'必要なファイルが存在しません: {f_path}')
         else:
+            file_size_bytes = Path(f_path).stat().st_size
+
+            # 容量チェック
+            if (file_size_bytes != f_info[1]):
+                r = False
+                print(f'ファイルの容量が異なります: {f_path} 計算容量:{file_size_bytes} 想定容量:{f_info[1]}')
+
             # crc32 チェック
             with open(f_path, 'rb') as f:
                 data = f.read()
+                
                 crc32 = binascii.crc32(data)
                 if (crc32 != f_info[0]):
                     r = False
-                    print(f'ファイルの整合性が取れません: {f_path} {hex(crc32)} {hex(f_info[0])}')
+                    print(f'ファイルの整合性が取れません: {f_path} 計算CRC:{hex(crc32)} 想定CRC:{hex(f_info[0])}')
     
     return r
