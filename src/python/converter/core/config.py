e44d131ba324783006813daf830617edf7087b7e
Prince-of-sea <94970975+Prince-of-sea@users.noreply.github.com>
2026-01-28 22:41:13 +0900

    python側での音声ビットレート記載を不要に

--- src/python/converter/core/config.py
+++ src/python/converter/core/config.py
@@ -46,16 +46,16 @@ class AppConfig:
     result_gba: Path
     base_gba: Path
 
+    # ===== 音声関連 =====
+    sound_quality_high: int = 0
+    sound_quality_low: int = 0
+    sound_quality_high_message: str = ""
+    sound_quality_low_message: str = ""
+
     # ===== 設定関連 =====
     bgm_high_quality: bool = False
     voice_on: bool = True
     debug_mode: bool = False
-
-    # ===== 設定関連 =====
-    sound_quality_high: int = 8738
-    sound_quality_low: int = 5734
-    sound_quality_high_message: str = f"高音質再生モード(声無し・{sound_quality_high}Hz)"
-    sound_quality_low_message: str = f"ボイス搭載モード(声アリ・{sound_quality_low}Hz)"
 
     # ===== プログレスバー進捗割合 =====
     progress_dict: dict = None
@@ -93,6 +93,29 @@ def set_gui_config(cfg: AppConfig) -> None:
         Path(cfg.debug_dir / 'scn').mkdir(parents=True, exist_ok=True)
     
     return
+
+
+def set_rom_audio_rate(cfg: AppConfig) -> list[int]:
+    """置いてあるROMのビットレート数値を取得してリストで返す"""
+    cwd = Path.cwd()
+
+    # ベースROM置き場
+    base_gba_dir = Path(cwd / "resources" / "base_gba")
+
+    # 代入用ビットレートリスト
+    rom_audio_rate_list = []
+
+    # ベースROM置き場からfor
+    for p in base_gba_dir.glob("base_[0-9]*.gba"):
+
+        # ビットレート取得
+        rate = int(p.stem[5:])
+
+        # リストに追加
+        rom_audio_rate_list.append(rate)
+    
+    # ソートして返却
+    return sorted(rom_audio_rate_list, reverse=True)
 
 
 def create_config(temp_dir: Path) -> AppConfig:
@@ -146,5 +169,9 @@ def create_config(temp_dir: Path) -> AppConfig:
             "join_binary_files": 100,
         },
     )
+
+    cfg.sound_quality_high, *_, cfg.sound_quality_low = set_rom_audio_rate(cfg)
+    cfg.sound_quality_high_message = f"高音質再生モード(声無し・{cfg.sound_quality_high}Hz)"
+    cfg.sound_quality_low_message = f"ボイス搭載モード(声アリ・{cfg.sound_quality_low}Hz)"
 
     return cfg
