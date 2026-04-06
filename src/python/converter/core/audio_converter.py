dd414476416fbe706d64615e7aeb5cc388992cde
Prince-of-sea <94970975+Prince-of-sea@users.noreply.github.com>
2026-04-07 00:02:53 +0900

    🎶 前後が途切れて聞こえる音源を修正+

--- src/python/converter/core/audio_converter.py
+++ src/python/converter/core/audio_converter.py
@@ -65,7 +65,7 @@ def run_sox_dummy(cfg: AppConfig, dummy_raw_path: Path) -> None:
 
     # 無音ファイル作成(音声再生後に、「データ上で次にあるファイル」の先頭が一瞬流れるバグがあるのでその解消用)
     # 次が流れてもそれが無音なら気づかれなくて済む、実害無い、とかいう雑な回避策
-    cmd = [cfg.sox_exe, '-n', '-c1', f'-r{rate}', '-B', '-b8', '-e', 'signed-integer', dummy_raw_path, 'trim', '0', '1']
+    cmd = [cfg.sox_exe, '-n', '-c1', f'-r{rate}', '-B', '-b8', '-e', 'signed-integer', dummy_raw_path, 'trim', '0', '1.3']
 
     # メイン処理 - コマンド実行
     subprocess.run(cmd, cwd = cfg.convert_dir, **subprocess_args())
